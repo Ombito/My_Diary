b332635d1ecb6fe5855eece14c3bca845260d95a
ombito <alvinombito09@gmail.com>
2023-07-27 22:14:43 +0300

    Complete my diary

--- script.js
+++ script.js
@@ -1,56 +1,71 @@
-let entry_list = documents.getElementById('entries');
-let postform = document.getElementById('postForm')
-let dataUrl = 'https://localhost:3000/diaryEntries'
+// Bring data from db
+    // fetch data
 
-fetch(dataUrl)
-.then(res => res.json())
-.then(data => displayImages(data))
-
-let displayImages = (data) => {
-    data.map(i => {
-        let entry = document.createElement('div')
-        entry.innerHTML = `
-        <h3>Title: ${i.title}</h3>
-        <h6>Date ${i.date}</h6>
-        <p>${i.content}</p>
-        `
-        entry_list.appendChild(entry)
-    })
-}
-
-//POST
-    //access the inputs value and store as vaeriables
-    //fetch
-        //define method
-        //define headers and body
-        //convert to json
-        //add event listener
-        //prevent default  
-
-    let addDiaryEntry = (event) => {
-        event.preventDefault()
-
-        let title = document.getElementById('title').value
-        console.log(title)
-        let date = document.getElementById('date').value
-        let experience = document.getElementById('experience').value
+    let  entry_list = document.getElementById('entries')
+    // GET
+    fetch("http://localhost:3000/diaryEntries")
+    .then(res => res.json())
+    .then(data => manipulateEntries(data));
+    
+    const manipulateEntries = (data) => {
+        data.map(i => {
+            let entry = document.createElement('div')
+            entry.innerHTML = `
+            <h3>Title :${i.title} </h3>
+            
+            <h5>Date :${i.date} </h5><br>
+            <p> ${i.content}</p>
+            `
+        entry.addEventListener('click', handleUpdate)
+        entry_list.appendChild(entry);
+    
+    
+        // PATCH
+    function handleUpdate() {
+        let updateContent = prompt("Update Experience")
+        let newObj = {
+            content: updateContent,
+        }
+        fetch(`http://localhost:3000/diaryEntries/${i.id}`,{
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json',
+                    Accept: 'application/json'
+        
+                     },
+            body: JSON.stringify(newObj)
+            
+    
+        })
+        window.location.reload();
     }
-
-    const entryObj = {
-        title: title,
-        date: date,
-        content: experience
+        })
+    
     }
-
-
-    fetch(dataUrl, {
-        method: 'POST',
-        header:{ 
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(entryObj)
-    })
-    .then(res => res.json)
-    .then(data => console.log(data))
-
-    postform.addEventListener('submit',addDiaryEntry)
+    // POST
+    // Post our experience
+        //access the inputs 
+    let addDiaryEntry = (e) =>{
+        e.preventDefault();
+        let title = document.getElementById('title').value;
+        let date = document.getElementById('date').value;
+        let experience = document.getElementById('experience').value;
+        
+        fetch("http://localhost:3000/diaryEntries",{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                Accept: 'application/json'} , 
+            body: JSON.stringify({
+                date: date,
+                title: title,
+                content: experience
+            })
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        form.reset();
+    
+        
+    }
+    let form = document.getElementsByTagName('form')[0]
+    form.addEventListener('submit',addDiaryEntry)
